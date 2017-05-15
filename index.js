947c335ec0540f502d81ed921750a6900250ea59
hellosylvee <helloimsylvia@gmail.com>
2017-05-15 11:45:40 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-const app = "I don't do much.";
+const madMaxGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller']
+
+ReactDOM.render(
+  <
+    MovieCard title="Mad Max: Fury Road"
+    poster="http://image.tmdb.org/t/p/w342/kqjL17yufvn9OVLyXYpvtyrFfak.jpg"
+    genres={ madMaxGenres }
+  />,
+  document.getElementById('main')
+);
+
+// When using default values for props
+// MovieCard.defaultProps = {
+//   poster: 'http://i.imgur.com/bJw8ndW.png'
+// };
+
+// When using ES2015:
+// const MovieCard = React.createClass({
+//   getDefaultProps() {
+//     return {
+//       poster: 'http://i.imgur.com/bJw8ndW.png'
+//     };
+//   },
+//   render() {
+//   }
+// })
